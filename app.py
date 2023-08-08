5890f10df603426b757fe3dbc9ae72195a79535b
Joey Liao <joey.liao0127@gmail.com>
2023-08-08 11:23:41 +0800

    member.index top bar done

--- app.py
+++ app.py
@@ -9,4 +9,9 @@ app = Flask(
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/member")
+def member():
+    return render_template("member.html")
+
 app.run(port=3000, debug=True, use_reloader=True)
